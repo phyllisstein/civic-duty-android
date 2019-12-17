00c12cfb710b6d0f96163010d8a1faa25cb40967
Sergey Morozov <sergey_morozov@discovery.com>
2019-12-17 13:22:38 -0500

    initial commit

---
+++ app/src/main/java/nyc/ignitelabs/civicduty/MainActivity.kt
@@ -0,0 +1,12 @@
+package nyc.ignitelabs.civicduty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
